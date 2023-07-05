436e904fc6eb3f63bce75ec0ec1d02f26436011a
amanecer <amanecer.luz.cabrera@gmail.com>
2023-07-04 21:18:24 -0400

    added files

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const movie_id = 725201
+
+const url = `${process.env.API_URL}/${movie_id}/recommendations`;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.API_KEY}`
+  }
+};
+
+axios.get(url, options)
+  .then(response => {
+    console.log(response.data);
+    const title = response.data.results[0].title;
+    console.log(title);
+  })
+  .catch(err => {
+    console.error('error:' + err);
+  });
